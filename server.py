6775f7fcbb83ca5ef8a2e2302232342ffc75f078
spiritualized <jeff.warren@gmail.com>
2018-04-11 16:28:50 -0400

    Fixed ordering on Saved tab

--- server.py
+++ server.py
@@ -109,7 +109,7 @@ def api():
 		other = format_results(results, user)
 
 		saved = []
-		results = db.session.query(UserStory).filter_by(token=user.token).order_by(Story.posted.desc()).all()
+		results = db.session.query(UserStory).filter_by(token=user.token)
 		for result in results:
 			saved.append(result.story_id)
 
